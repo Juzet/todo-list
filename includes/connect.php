1389ccc89bbcf90010b12436d623c2240f91ea2a
Juzet Arana <arana.juzet@gmail.com>
2015-04-16 07:47:29 -0700

    finished video 5 milestone 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 // this is important becuase it connects to the database
 // localhost has to be first because you are using php and that uses localhost
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
-	if (mysqli->connect_error) {
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+	if ($mysqli->connect_error) {
 		die('Connect Error(' . $mysqli_>connect_errno .')'
 			. $mysqli->connect_error);
 }
