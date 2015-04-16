27217f3c93408045ef634adcab5c04923e3696de
Juzet Arana <arana.juzet@gmail.com>
2015-04-16 07:39:16 -0700

    finished video 4

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+// this is important becuase it connects to the database
+// localhost has to be first because you are using php and that uses localhost
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	if (mysqli->connect_error) {
+		die('Connect Error(' . $mysqli_>connect_errno .')'
+			. $mysqli->connect_error);
+}
+else {
+	echo "Connection made";
+}
+$mysqli->close();
+
+?>
